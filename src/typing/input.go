758cd206222596179ad8a9d80e15014fb2c919d4
khwask <kawasaki.masahiro@gmail.com>
2018-04-27 23:37:16 +0900

    add typing package

--- src/typing/input.go
+++ src/typing/input.go
@@ -1,10 +1,12 @@
-package main
+package typing
 
 import (
 	"bufio"
 	"io"
 )
 
+// ユーザからの入力を待ち受ける
+// 入力された内容は、戻り値のチャネルに送る
 func input(r io.Reader) <-chan string {
 	ch := make(chan string)
 	go func() {
